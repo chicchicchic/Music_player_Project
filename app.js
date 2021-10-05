db085857b1f3813c00a3c64bcfc06968484ab501
chicchicchic <64607059+chicchicchic@users.noreply.github.com>
2021-10-05 08:50:55 +0700

    Add source code Music player project, ...7:24

---
+++ app.js
@@ -0,0 +1,97 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const app = {
+    songs: [
+        {
+            name: 'COMFY (돌아온 탕자) - COUCH WORSHIP',
+            singer: 'worship 1',
+            path: './assets/music/song1.mp3',
+            image: './assets/img/song1.PNG'
+        },
+        {
+            name: '기뻐해 I rejoice | 제이어스 J-US',
+            singer: 'worship 2',
+            path: './assets/music/song2.mp3',
+            image: './assets/img/song2.PNG'
+        },
+        {
+            name: 'ON (Feat. 김아연) - 유지연',
+            singer: 'worship 3',
+            path: './assets/music/song3.mp3',
+            image: './assets/img/song3.PNG'
+        },
+        {
+            name: '여호와의 집으로 올라가 - 마커스워십',
+            singer: 'worship 4',
+            path: './assets/music/song4.mp3',
+            image: './assets/img/song4.PNG'
+        },
+        {
+            name: '그곳에서 안무 영상- 서하얀',
+            singer: 'worship 5',
+            path: './assets/music/song5.mp3',
+            image: './assets/img/song5.PNG'
+        },
+        {
+            name: '함부영 - 나의 노래',
+            singer: 'worship 6',
+            path: './assets/music/song6.mp3',
+            image: './assets/img/song6.PNG'
+        },
+        {
+            name: 'In your love-함부영',
+            singer: 'worship 7',
+            path: './assets/music/song7.mp3',
+            image: './assets/img/song7.PNG'
+        },
+        {
+            name: '아무도 없을 때 (정결의 노래) -지선',
+            singer: 'worship 8',
+            path: './assets/music/song8.mp3',
+            image: './assets/img/song8.PNG'
+        },
+        {
+            name: 'Maranatha || 마라나타 || Phạm Thị Yến Nga',
+            singer: 'worship 9',
+            path: './assets/music/song9.mp3',
+            image: './assets/img/song9.PNG'
+        },
+        {
+            name: 'Like You Love Me-Tauren Wells',
+            singer: 'worship 10',
+            path: './assets/music/song10.mp3',
+            image: './assets/img/song10.PNG'
+        },
+        {
+            name: '마커스-길',
+            singer: 'worship 11',
+            path: './assets/music/song11.mp3',
+            image: './assets/img/song11.PNG'
+        },
+        {
+            name: 'みんなで賛美しよう',
+            singer: 'worship 12',
+            path: './assets/music/song12.mp3',
+            image: './assets/img/song12.PNG'
+        },
+        {
+            name: '小さな祈り',
+            singer: 'worship 13',
+            path: './assets/music/song13.mp3',
+            image: './assets/img/song13.PNG'
+        },
+        {
+            name: 'David - 비와이(Bewhy)',
+            singer: 'worship 14',
+            path: './assets/music/song14.mp3',
+            image: './assets/img/song14.PNG'
+        },
+        {
+            name: '은혜로 살게 하시네 (Feat. 백소라)',
+            singer: 'worship 15',
+            path: './assets/music/song15.mp3',
+            image: './assets/img/song15.PNG'
+        }
+    ],
+}
