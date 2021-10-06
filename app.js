e981d28b51306d9ffbb57586c5b86447b43f82e7
chicchicchic <64607059+chicchicchic@users.noreply.github.com>
2021-10-06 10:07:00 +0700

    Add source code Music player project, ...50:35

--- app.js
+++ app.js
@@ -15,9 +15,27 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+// Đây là những biến có thể lặp lại nên khai báo global
+const player = $('.player');
+const cd =$('.cd');
+// Biến này dùng để render tên của bài hát
+const heading = $('header h2');
+// Biến này để đổi url bài hát hiện tại
+const cdThumb = $('.cd-thumb');
+// Biến này để lấy bài hát
+const audio = $('#audio');
+// Nút play/pause 
+const playBtn = $('.btn-toggle-play');
+// Biến này là tiến độ bài hát
+const progress = $('#progress');
+
+
+
 const app = {
     // currentIndex: 0 nhầm muốn lấy ra chỉ mục đẩu tiên để xử lý, vì phần audio chưa có bài hát nào, nhưng ngay khi app tải ra mình phải load đc bài hát đầu tiên
     currentIndex: 0,
+    // isPlaying là property của app, mặc định là chưa phát nhạc (User chưa click Play Btn)
+    isPlaying: false,
     songs: [
         {
             name: 'COMFY (돌아온 탕자) - COUCH WORSHIP',
@@ -139,12 +157,32 @@ const app = {
         })
     },
     // handleEvents này là xử lý tất cả sự kiện của app
-    handleEvents: function() {
-        const cd =$('.cd');
+    handleEvents: function() { 
+        // Biến _this này là app, không dùng thằng this ở dưới event playBtn.onclick vì nó sẽ nghĩ this ở đó là 'playBtn' chứ không phải 'app' 
+        const _this = this;
+
         // biến cdWidth là width hiện tại của nó
         const cdWidth = cd.offsetWidth;
 
-        // Lắng nghe event kéo
+
+        // Xử lý CD quay / dừng 
+        // (search 'Animation Web API' để tìm hiểu, sẽ biết đc 1 keyframe dc tạo ra từ JS)
+        const cdThumbAnimate = cdThumb.animate([
+            // Quay 360 độ
+            { transform: 'rotate(360deg)' }
+        ], {
+            // Đối số thứ 2 của cdThumb.animate là 1 option sẻ quyết định nó quay như nào
+            // Quay trong 10 seconds
+            duration: 10000,
+            // iterations nghĩa là loop bao nhiu lần, biến Infinity trong JS là vô hạn
+            iterations: Infinity
+        })
+        // Sau khi add animate thì cho nó pause từ dầu luôn chứ không cho nó quay, khi xử lý User play/pause thì nó mới quay/ko quay tiếp 
+        // Nếu ko pause thì khi app dc load là nó quay luôn, mặc dù user chưa click play gì hết
+        cdThumbAnimate.pause();
+
+
+        // Lắng nghe event kéo, xử lý phóng to/thu nhỏ cd
         document.onscroll = function() {
             // Kiểm tra nó lắng nghe event kéo
             // console.log(window.scrollY);
@@ -162,18 +200,76 @@ const app = {
             cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0;
             // Set Css cho cd mờ dần khi thu nhỏ và đậm dần khi phóng to
             cd.style.opacity = newCdWidth / cdWidth; 
+        }
+
+        // Xữ lý khi User click Play btn
+        playBtn.onclick = function() {
+            if(_this.isPlaying) {
+                // Nếu là isPlaying (mặc định false) thì song sẽ pause
+                audio.pause();
+            }else {
+                // Ngược lại thì song sẽ play
+                audio.play();           
+            }
+
+
+            // console.log ra để nhìn phương thức của cdThumbAnimate (trong__proto__), ở đây ta dùng phương thức play, pause
+            // console.log(cdThumbAnimate);
+
+            // Khi song đc playing
+            audio.onplay = function() {
+                _this.isPlaying = true;
+                // Khi thêm class 'playing' này vào Selector biến 'player' thì khi User click vào nút play thì nó sẽ chuyển sang icon pause
+                player.classList.add('playing');
+
+                // Khi play thì cdThumbAnimate quay
+                cdThumbAnimate.play();
+            }
+
+
+            // Khi song ko đc playing (Pause)
+            audio.onpause = function() {
+                _this.isPlaying = false;
+                // Khi thêm class 'playing' này vào Selector biến 'player' thì khi User click vào nút play thì nó sẽ chuyển sang icon pause
+                player.classList.remove('playing');
+
+                // Khi ko play thì cdThumbAnimate ko quay
+                cdThumbAnimate.pause();
+            }
+
+
+            // Khi tiến độ bài hát thay đổi 
+            audio.ontimeupdate = function() {
+                // Check nếu là duration ko phải NaN thì
+                if(audio.duration) {
+                    // currentTime là time hiện tại (tính giây)
+                    // duration là tổng time của bài hát (tính giây)
+                    // Chia tỷ lệ, tính toán và làm tròn dưới
+                    const progressPercent = Math.floor(audio.currentTime / audio.duration * 100);
+                    // Có thể console.log ra để xem
+                    // console.log(progressPercent);
+
+                    // Khi progressPercent dc chia tỷ lệ và tính toán thí value của progress cũng thay đổi (xem bên Html cách set value), lúc này thanh trược sẽ trược theo tiến độ bài hát
+                    progress.value = progressPercent;
+
+                }
+            }
+
+            
+            // Xử lý khi User tua bài hát
+            progress.onchange = function(e) {
+                // Công thức: Tổng số giây bài hát / 100 * giá trị User thay đổi
+                // Có thể console.log ra để xem 
+                // console.log(audio.duration / 100 * e.target.value);
+
+                const seekTime = audio.duration / 100 * e.target.value;
+                audio.currentTime = seekTime;
+            }
 
         }
     },
     // Hàm load bài hát hiện tại
     loadCurrentSong: function() {
-        // Biến này dùng để render tên của bài hát
-        const heading = $('header h2');
-        // Biến này để đổi url bài hát hiện tại
-        const cdThumb = $('.cd-thumb');
-        // Biến này để lấy bài hát
-        const audio = $('#audio');
-
         heading.textContent = this.currentSong.name;
         cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
         audio.src = this.currentSong.path;
