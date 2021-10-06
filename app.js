008f3c5dc9426ed3d2010e26ec8dc1095b5c3bbf
chicchicchic <64607059+chicchicchic@users.noreply.github.com>
2021-10-06 20:15:29 +0700

    Add source code Music player project, ...full

--- app.js
+++ app.js
@@ -15,6 +15,9 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+
+const PLAYER_STORAGE_KEY = 'KHANH';
+
 // Đây là những biến có thể lặp lại nên khai báo global
 const player = $('.player');
 const cd =$('.cd');
@@ -34,6 +37,10 @@ const nextBtn = $('.btn-next');
 const prevBtn = $('.btn-prev');
 // Biến này là random song
 const randomBtn = $('.btn-random');
+// Biến này là repeat song
+const repeatBtn = $('.btn-repeat');
+// Biến này là biến Selector block playlist
+const playlist = $('.playlist');
 
 
 
@@ -44,8 +51,16 @@ const app = {
     currentIndex: 0,
     // isPlaying là property của app, mặc định là chưa phát nhạc (User chưa click Play Btn)
     isPlaying: false,
-    // isRandom là property của app, mặc định là chưa dc bật nên chưa hiện màu (User chưa click random Btn)
+    // isRandom là property của app, mặc định là chưa dc bật nên chưa hiện màu và chưa kích hoạt (User chưa click random Btn)
     isRandom: false,
+    // isRepeat là property của app, mặc định là chưa dc bật nên chưa hiện màu và chưa kích hoạt (User chưa click repeat Btn)
+    isRepeat: false,
+    // setting là để nó setting nhưng 4 cái cần giữ lại, ko bị mất đi khi refresh trình duyệt
+    // Nếu mặc định mà ko có Key thì sẽ lấy 1 Object
+    // gõ 'app.config' ở tab console để kiểm tra
+    // get là lấy ra
+    // get là lấy ra thì parse chuyển thành Object
+    config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY)) || {},  
     songs: [
         {
             name: 'COMFY (돌아온 탕자) - COUCH WORSHIP',
@@ -138,10 +153,18 @@ const app = {
             image: './assets/img/song15.PNG'
         }
     ],
+    setConfig: function(key, value) {
+        this.config[key] = value;
+        // set là thêm vào
+        // set vào thì chuyển sang chuỗi
+        localStorage.setItem(PLAYER_STORAGE_KEY, JSON.stringify(this.config));
+    },
     render: function() {
-        const htmls = this.songs.map(song => {
+        const htmls = this.songs.map((song, index) => {
+            // ${index === this.currentIndex ? 'active' : ''} nghĩa là nếu index === this.currentIndex thì add thêm class 'active' vào item, ngược lại thì ko thêm gì cả
+            // Attribute 'data-index' đưa vào là để đưa vào index của từng song, nhầm mục đích khi User click vào item nào thì chỉ cần lấy đúng index của item đó là dc, để chuyển song khi User click vào song khác bài hát đang play
             return `
-                <div class="song">
+                <div class="song ${index === this.currentIndex ? 'active' : ''}" data-index="${index}">
                     <div class="thumb" style="background-image: url('${song.image}')">
                     </div>
                     <div class="body">
@@ -155,7 +178,7 @@ const app = {
             `
         })
         // Join vào block html cần render
-        $('.playlist').innerHTML = htmls.join('');
+        playlist.innerHTML = htmls.join('');
     },
     // Tất cả định nghĩa thuộc tính cho Object sẽ đưa vào đầy
     defineProperties: function() {
@@ -285,6 +308,11 @@ const app = {
                 _this.nextSong()
             }
             audio.play()
+
+            // render lại khi song dc phát hay next hay prev thì sẽ đc add class 'active' vào item để item bài hát dc đổi màu để User biết bài nào đang dc play
+            _this.render()
+            // Tự kéo tới bài hát đang phát
+            _this.scrollToActiveSong()
         }
 
 
@@ -296,23 +324,99 @@ const app = {
                 _this.prevSong()
             }
             audio.play()
+
+            // render lại khi song dc phát hay next hay prev thì sẽ đc add class 'active' vào item để item bài hát dc đổi màu để User biết bài nào đang dc play
+            _this.render()
+            // Tự kéo tới bài hát đang phát
+            _this.scrollToActiveSong()
         }
 
 
         // Khi User click random Btn, tắt/bật random Btn
         randomBtn.onclick = function(e) {
-            // Đảo ngược lại chính nó
+            // Đảo ngược lại chính nó, chuyển false thành true
             _this.isRandom = !_this.isRandom;
+
+            _this.setConfig('isRandom', _this.isRandom);
+
             // Dùng toggle nếu boolean lả true thì sẽ add class 'active', nếu là false thì sẽ remove class 'active'
-            randomBtn.classList.toggle('active', _this.random);
+            randomBtn.classList.toggle('active', _this.isRandom);
+        }
+
+        
+        // Khi user click repeat Btn thì song sẽ tự phát lại khi kết thúc song
+        repeatBtn.onclick = function() {
+            // Chuyển false thành true
+            _this.isRepeat = !_this.isRepeat;
+
+            _this.setConfig('isRepeat', _this.isRepeat);
+
+            repeatBtn.classList.toggle('active', _this.isRepeat);
+
         }
 
 
-        // Xử lý phát bài hát kế tiếp khi song kết thúc (audio ended)
+        // Xử lý phát bài hát kế tiếp khi song kết thúc (audio ended) hay là phát lại song hiện tại khi User click repeatBtn
         audio.onended = function() {
-            // Tự nó click vào nextBtn khi song nào đó kết thúc
-            nextBtn.click()
+            // Nếu repeat thì phát lại song, ngược lại nếu ko dc click repeat thì phát bài tiếp theo
+            if(_this.isRepeat){
+                audio.play()
+            }else{
+                // Tự nó click vào nextBtn khi song nào đó kết thúc
+                nextBtn.click()
+            }
         }
+
+
+        // Lắng nghe hành vi click vào play click
+        playlist.onclick = function(e) {
+            const songNode = e.target.closest('.song:not(.active)');
+
+
+            // e.target là trả về cái mà User click trúng, Vd như click vào tên song trong item của playlist thì trả về tên, click auth thì trả về auth v.v
+            // console.log e.target.closest() hoặc tự search closest mà tìm hiểu
+            // Closest đơn giản là trả về element là chính nó hoặc thẻ cha chứa nó, nếu ko tìm thấy element thì trả về null
+            // Check có phải là class 'song' hay ko, hoặc click vào con của nó thì nó cũng đúng
+            // Ở đây thì chỉ bắt sự kiện vào song nào ko có class 'active' thôi vì nó đang active (playing) thì bấm vào nó làm gì, chỉ bấm những song khác cho nó chuyển song
+            // Hoặc nếu e.target mà có class 'option' thì mới bắt sự kiện vỉ nút option này là để User click vào tùy chọn sau này
+            if( songNode || e.target.closest('.option')) {
+                // Xử lý click vào song
+                if(songNode) {
+                    // Console.log ra để nhìn nó getAttribute 'data-index'
+                    // console.log(songNode.getAttribute('data-index'));
+                    // Viết gọn gàn hơn là dataset.index thay cho getAttribute('data-index') vì vốn dĩ data-index là dùng dataset, vì ta ghi data-index là có ý đồ thui
+                    // console.log(songNode.dataset.index);
+
+                    // Set currentIndex là index của item mà User click (ngoại trừ song đang play)
+                    // Note: currentIndex của ta là Number nhưng gán "_this.currentIndex = songNode.dataset.index" là nó hỉu là chuỗi, nên bắt buộc ta phải ta phải chuyển nó về Number bằng cách "Number(songNode.dataset.index)"
+                    _this.currentIndex = Number(songNode.dataset.index);
+                    // Load currentSong sau khi currentIndex dc update 
+                    _this.loadCurrentSong()
+                    // render lại
+                    _this.render()
+                    // Và cuối cùng là play song
+                    audio.play()
+                }
+
+                // Xử lý click vào option
+                if(e.target.closest('.option')) {
+
+                }
+                
+            }
+
+        }
+    },
+    // Tự kéo tới bài hát đang phát
+    // Search phương thức scroll into view element để xem các phương thức của nó
+    scrollToActiveSong: function() {
+        setTimeout(() => {
+            // Chọn cách truyền vào scrollItoView 1 Object
+            $('.song.active').scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+            })
+        }, 500)
     },
     // Hàm load bài hát hiện tại
     loadCurrentSong: function() {
@@ -322,6 +426,10 @@ const app = {
 
         // console.log ra để thấy nó selector
         console.log(heading, cdThumb, audio);
+    },
+    loadConfig: function() {
+        this.isRandom = this.config.isRandom;
+        this.isRepeat = this.config.isRepeat;
 
     },
     // Nút chuyển bài hát kế tiếp
@@ -362,6 +470,9 @@ const app = {
         this.loadCurrentSong();
     },
     start: function() {
+        // Gán cấu hình từ config vào 'app' 
+        this.loadConfig()
+
         // Định nghĩa các thuộc tính cho Object
         this.defineProperties()
 
@@ -373,6 +484,10 @@ const app = {
 
         // Render playlist
         this.render()
+
+        // Hiển thị trạng thái ban đầu của button repeat và random
+        randomBtn.classList.toggle('active', this.isRandom);
+        repeatBtn.classList.toggle('active', this.isRepeat);
     }
 }
 app.start()
